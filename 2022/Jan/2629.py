b85fe0c012ca6d1a8dd505c06285da915e1dda09
Y-Joo <wldudwn99@gmail.com>
2022-01-11 15:52:34 +0900

    20220111

--- 2022/Jan/2629.py
+++ 2022/Jan/2629.py
@@ -1,28 +1,24 @@
+import collections
 n = int(input())
 bells = list(map(int, input().split()))
 k = int(input())
 marvels = list(map(int, input().split()))
 l = 40000 + 1
-dp = [False for _ in range(l)]
+dp = collections.defaultdict()
 dp[0] = True
 for i in bells[::-1]:
-    visited = [False for _ in range(l)]
-    for j in range(l):
-        if dp[j] and not visited[j]:
-            tmp = j
-            while tmp < l and dp[tmp] and not visited[tmp]:
-                tmp += i
-            if tmp < l:
-                dp[tmp] = True
-                visited[tmp] = True
-            tmp = j
-            while tmp > 0 and dp[tmp] and not visited[tmp]:
-                tmp -= i
-            if tmp > 0:
-                dp[tmp] = True
-                visited[tmp] = True
+    append_list = []
+    for j in dp.keys():
+        if (j + i) not in dp.keys():
+            append_list.append(j + i)
+        if (j - i) not in dp.keys():
+            append_list.append(j - i)
+    for a in append_list:
+        dp[a] = True
+result = []
 for marvel in marvels:
-    if dp[marvel]:
-        print("Y", end=" ")
+    if marvel in dp.keys() and dp[marvel]:
+        result.append("Y")
     else:
-        print("N", end=" ")
+        result.append("N")
+print(*result)
